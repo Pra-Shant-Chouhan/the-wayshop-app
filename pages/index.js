8c2e9cf4e3c162b5e5932935eb037a94dae901ad
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2022-03-06 14:56:18 +0530

    In Home page ShopCategories added by the help of reducer

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
+import Crouserlslider from '../components/HeroSlider/Crouserlslider'
+import ShopCategories from '../components/ShopCategories/ShopCategories'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
-      
+      <Crouserlslider />
+      <ShopCategories/>
     </>
   )
 }
