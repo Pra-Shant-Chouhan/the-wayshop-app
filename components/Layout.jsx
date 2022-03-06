164e723cb38f0b9f929a3375118a2b34a9f4425f
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2022-03-06 11:20:00 +0530

    Footer added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from './Layout/coponentHedFoot/Footer'
 import Header from './Layout/coponentHedFoot/Header'
 
 function Layout({children}) {
@@ -6,6 +7,7 @@ function Layout({children}) {
     <>
       <Header />
       {children}
+      <Footer/>
       <a href="#" id="back-to-top" title="Back to top" style={{ display: "none" }}>&uarr;</a>
 
       
