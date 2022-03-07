2fb1826ebeb03690a94f79590080e3ca9666bad7
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2022-03-07 13:28:55 +0530

    Cart reducer added

--- root-reducer.js
+++ root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import shopCategoriesReducer from "./components/ShopCategories/ShopCatergories.reducer";
+import cartReducer from "./reducers/cart";
 export const rootReducer = combineReducers({
   
   shopCategories: shopCategoriesReducer,
+  cart: cartReducer
 });
