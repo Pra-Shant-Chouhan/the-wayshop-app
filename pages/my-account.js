3edb9fdaca0ad87d16bef3066def1dfe6d18168c
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2022-03-07 13:23:03 +0530

    Link added in header filte adn create its pages

---
+++ pages/my-account.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Myaccount() {
+  return (
+    <div>my-account</div>
+  )
+}
---
+++ pages/whishlist.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function whishlist() {
+  return (
+    <div>whishlist</div>
+  )
+}
