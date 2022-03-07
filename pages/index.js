ffbc2c3a306268dc934b80dde67cac64f0984f60
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2022-03-07 13:28:01 +0530

    FeturedProducts added

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 
+import FeturedProducts from '../components/FeturedPorducts/FeturedProducts'
 import Crouserlslider from '../components/HeroSlider/Crouserlslider'
 import ShopCategories from '../components/ShopCategories/ShopCategories.container'
-import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
     <>
       <Crouserlslider />
-      <ShopCategories/>
+      <ShopCategories />
+      <FeturedProducts/>
     </>
   )
 }
